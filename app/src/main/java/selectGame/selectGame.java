0ebf2d45bebd9ba77af1f2193d6255ef62f7af69
Catnip4242 <42363024+Catnip4242@users.noreply.github.com>
2018-08-27 11:32:01 +1200

    hooking select_game.xml to the selectGame activity

--- app/src/main/java/selectGame/selectGame.java
+++ app/src/main/java/selectGame/selectGame.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.negar.snakesladders.R;
+
 
 public class selectGame extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_game);
     }
 
 
