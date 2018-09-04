1683c6a0b761dc6061d0a3e08fa14f883e9a7c70
Negar <nemote95@gmail.com>
2018-09-05 11:46:20 +1200

    optimized showing avatar and board and movement updates and finishing page

--- app/src/main/java/com/example/negar/snakesladders/Board.java
+++ app/src/main/java/com/example/negar/snakesladders/Board.java
@@ -10,14 +10,16 @@ import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
+
+
 
 public class Board extends AppCompatActivity {
     //board canvas settings
@@ -30,11 +32,14 @@ public class Board extends AppCompatActivity {
     private static final int OFFSET = 10;
     private int mColorBackground;
     private int mColorRectangle;
+    private int sqHeight,sqWidth;
 
 
     //get the size from indent later
     private int boardSize=5;
     private int userTile=1;
+    private int userPrevTile=1;
+    private int userGoal=1;
     List<List<Integer>> boardTilesPoints = new ArrayList<List<Integer>>();
     
     
@@ -62,13 +67,26 @@ public class Board extends AppCompatActivity {
         );
 
         mImageView = (ImageView) findViewById(R.id.myimageview);
-        //drawSomething(mImageView);
 
+        mImageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Boolean mMeasured=false;
+
+                if (!mMeasured) {
+
+                    // Here your view is already layed out and measured for the first time
+                    drawSomething(mImageView);
+                    mMeasured = true; // Some optional flag to mark, that we already got the sizes
+
+                }
+            }
+        });
 
 
     }
 
-    public void drawSomething(View view) throws InterruptedException {
+    public void drawSomething(View view)  {
 
         int vWidth = view.getWidth();
         int vHeight = view.getHeight();
@@ -80,8 +98,8 @@ public class Board extends AppCompatActivity {
 
 
     // Change the color by subtracting an integer.
-        int sqWidth = (vWidth - OFFSET) / boardSize - OFFSET;
-        int sqHeight = (vHeight - OFFSET) / boardSize - OFFSET;
+        sqWidth = (vWidth - OFFSET) / boardSize - OFFSET;
+        sqHeight = (vHeight - OFFSET) / boardSize - OFFSET;
 
         mPaintText.setTextSize(sqWidth/3);
 
@@ -99,17 +117,8 @@ public class Board extends AppCompatActivity {
 
             }
         }
-
-        //user movement;use while statement later
-
-        if (userTile!=boardSize*boardSize){
-            showAvatarInTile(userTile,sqWidth,sqHeight);
-        }
-        else{
-            userTile=0;// got congratulation page!
-        }
-
-        //view.invalidate();
+        showAvatarInTile();
+                //view.invalidate();
     }
 
     int pointToTile(int x,int y){
@@ -117,28 +126,52 @@ public class Board extends AppCompatActivity {
             if (x>boardTilesPoints.get(i).get(0) & x<boardTilesPoints.get(i).get(2) & y>boardTilesPoints.get(i).get(1) & y<boardTilesPoints.get(i).get(3))
             {
                 return i;
-
             }
         }
         return -1;
     }
 
     Point tileToPoit(int t){
-        t=t/boardSize+((t%boardSize-1)*boardSize);
+        if (t%boardSize==0){
+            t=t/boardSize+(boardSize-1)*boardSize-1;
+        }
+        else{
+            t=t/boardSize+((t%boardSize-1)*boardSize);
+        }
+
         int x= boardTilesPoints.get(t).get(0);
         int y=boardTilesPoints.get(t).get(1);
         Point p=new Point(x,y);
         return p;
     }
 
-    void showAvatarInTile(int tile,int sqWidth,int sqHeight){
-        Point p =tileToPoit(tile);
+    void showAvatarInTile(){
+        Point p =tileToPoit(userTile);
+        Point prev=tileToPoit(userPrevTile);
+
+        //clear_prev
+        mRect.set(prev.x+sqWidth/4, prev.y+sqHeight/2, prev.x+3*sqWidth/4, prev.y + sqHeight);
+        mCanvas.drawRect(mRect,mPaint);
+
 
         Bitmap avatar = BitmapFactory.decodeResource(getResources(), R.drawable.avatar);
-
         Bitmap resizedBitmap = Bitmap.createScaledBitmap(avatar, sqWidth/2, sqHeight/2, true);
-
         mCanvas.drawBitmap(resizedBitmap,p.x+sqWidth/4,p.y+sqHeight/2,mPaint);
+    }
+
+    void manageUserMovement(){
+
+        if (userTile>=boardSize*boardSize & userGoal>=boardSize*boardSize){
+            //celebrate
+
+            Intent finish = new Intent(this,Finish.class);
+            startActivityForResult(finish, 1);
+
+
+        }
+        else {
+            showAvatarInTile();
+        }
     }
 
     void getDiceNumber(View view){
@@ -150,7 +183,14 @@ public class Board extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == ROLL_Dice_REQUEST) {
             if (resultCode == RESULT_OK) {
+                userPrevTile=userTile;
+                userGoal+=data.getIntExtra("diceNumber",1);
+
+
+                //should be later in gps listener
                 userTile+=data.getIntExtra("diceNumber",1);
+                manageUserMovement();
+
             }
         }
     }
---
+++ app/src/main/java/com/example/negar/snakesladders/Finish.java
@@ -0,0 +1,19 @@
+package com.example.negar.snakesladders;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class Finish extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+
+    void startAgain(View view){
+
+    }
+
+}
